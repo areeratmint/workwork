681f88e56acd7c3c036e183317bf5beb544f7e6d
areeratmint <aree_rat@hotmail.co.th>
2014-10-15 23:40:15 +0700

    edis1

---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ก๋วยเตี๋ยวบ้านลายจุดข้างวัง</title>
+
+<link href="http://se.ict.up.ac.th/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="http://se.ict.up.ac.th/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+<link href="http://se.ict.up.ac.th/bootstrap/js/bootstrap.min.js" rel="stylesheet">
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  
+ 
+  
+<center>
+<font size="6">ก๋วยเตี๋ยวบ้านลายจุดข้างวัง</font>
+</center> 
+  <table class="table">
+    <thead>
+    <tr>
+      <td width="61">เส้น</td>
+      <td width="50">ต้มยำ</td>
+      <td width="39">น้ำใส</td>
+      <td width="83">จำนวน</td>
+      <td width="76">ปลาหมึก</td>
+      <td width="101">กุ้ง</td>
+      <td width="83">ไข่ยางมะตูม</td>
+      <td width="140">หมายเหตุ</td>
+    </tr>
+     </thead>
+    <tr>
+      <td>หมี่ขาว</td>
+      <td><p>
+        <input type="checkbox" name="tumyum1" id="tumyum1" />
+        <br />
+      </p></td>
+      <td><input type="checkbox" name="water1" id="water1" /></td>
+      <td>
+        <select name="count1" id="count1">
+        <option value="number">0</option>
+          <option value="number1">1</option>
+          <option value="number2">2</option>
+          <option value="number3">3</option>
+          <option value="number4">4</option>
+          <option value="number5">5</option>
+          <option value="number6">6</option>
+          <option value="number7">7</option>
+          <option value="number8">8</option>
+          <option value="number9">9</option>
+          <option value="number10">10</option>
+      </select>
+        /ชาม</td>
+      <td><select name="count_fish1" id="count_fish1">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ตัว</td>
+      <td><select name="count_kun1" id="count_fish6">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+      /ตัว</td>
+      <td><select name="count_egg1" id="count_kun5">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ฟอง</td>
+      <td><input type="text" name="txt_note1" id="textfield" /></td>
+    </tr>
+    <tr>
+      <td>เส้นเล็ก</td>
+      <td><p>
+        <input type="checkbox" name="tumyum2" id="tumyum2" />
+        <br />
+      </p></td>
+      <td><input type="checkbox" name="water2" id="water2" /></td>
+      <td><select name="count2" id="count2">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ชาม</td>
+      <td><select name="count_fish2" id="count_fish2">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+      /ตัว</td>
+      <td><select name="count_kun2" id="count_kun">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+/ตัว</td>
+      <td><select name="count_egg2" id="count_egg">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+/ฟอง</td>
+      <td><input type="text" name="textfield" id="textfield2" /></td>
+    </tr>
+    <tr>
+      <td>เส้นใหญ่</td>
+      <td><input type="checkbox" name="tumyum3" id="tumyum3" /></td>
+      <td><input type="checkbox" name="water3" id="water3" /></td>
+      <td><select name="count3" id="count3">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ชาม</td>
+      <td><select name="count_fish3" id="count_fish3">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ตัว</td>
+      <td><select name="count_kun3" id="count_kun2">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+/ตัว</td>
+      <td><select name="count_egg3" id="count_egg2">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+/ฟอง</td>
+      <td><input type="text" name="textfield2" id="textfield3" /></td>
+    </tr>
+    <tr>
+      <td>บะหมี่</td>
+      <td><input type="checkbox" name="tumyum4" id="tumyum4" /></td>
+      <td><input type="checkbox" name="water4" id="water4" /></td>
+      <td><select name="count4" id="count4">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ชาม</td>
+      <td><select name="count_fish4" id="count_fish4">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ตัว</td>
+      <td><select name="count_kun4" id="count_kun3">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+/ตัว</td>
+      <td><select name="count_egg4" id="count_egg3">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+/ฟอง</td>
+      <td><input type="text" name="textfield3" id="textfield4" /></td>
+    </tr>
+    <tr>
+      <td>วุ้นเส้น</td>
+      <td><input type="checkbox" name="tumyum5" id="tumyum5" /></td>
+      <td><input type="checkbox" name="water5" id="water5" /></td>
+      <td><select name="count5" id="count5">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ชาม</td>
+      <td><select name="count_fish5" id="count_fish5">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+        /ตัว</td>
+      <td><select name="count_kun5" id="count_kun4">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+/ตัว</td>
+      <td><select name="count_egg5" id="count_egg4">
+      <option value="number0">0</option>
+        <option value="number1">1</option>
+        <option value="number2">2</option>
+        <option value="number3">3</option>
+        <option value="number4">4</option>
+        <option value="number5">5</option>
+        <option value="number6">6</option>
+        <option value="number7">7</option>
+        <option value="number8">8</option>
+        <option value="number9">9</option>
+        <option value="number10">10</option>
+      </select>
+/ฟอง</td>
+      <td><input type="text" name="textfield4" id="textfield5" /></td>
+    </tr>
+  </table>
+  <p>
+   <input
+	type="button" class="btn btn-primary" id="btm_sum" name="sum" value="สั่ง">
+  </p>
+  
+  <p><br />
+  </p>
+</form>
+
+
+
+
+
+
+</body>
+</html>
