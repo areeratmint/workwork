e8e17c19e5b7ea4d62fcee9ec278c4a231bd336a
wanasat <s.birdtoday@hotmail.com>
2014-10-16 00:36:40 -0700

    edd

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>ก๋วยเตี๋ยวบ้านลายจุดข้างวัง</title>
+<title>ก๋วยเตี๋ยวบ้านลายจุดข้างวัง55+</title>
 
 <link href="http://se.ict.up.ac.th/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="http://se.ict.up.ac.th/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
